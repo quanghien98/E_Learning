4172705834021c3e07f0ec688193aa4fdf4f8216
phu-pc <Quocphu123>
2019-10-07 19:44:56 +0700

    def

--- src/Reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { coursesReducer } from "./coursesReducer";
-
-
-const rootReducer = combineReducers({
-    courses: coursesReducer,
-})
-
-export default rootReducer
